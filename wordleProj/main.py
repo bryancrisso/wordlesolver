a3a5b0af55a112afe2347fed9b95363a5bef3ab8
Bryan Abi Karam <bryancrisso@outlook.com>
2022-03-22 19:58:51 +0000

    Added JSON reading and writing
    Fixed the tuple error

--- wordleProj/main.py
+++ wordleProj/main.py
@@ -1,4 +1,10 @@
-import string
+import string, json
+
+#generating combinations
+# import itertools
+# choices = ["a","g","gr"]
+# _choices = [choices]*5
+# combinations = [p for p in itertools.product(*_choices)]
 
 def parseWords(filename):
     returnList = []
@@ -7,17 +13,26 @@ def parseWords(filename):
             returnList.append(line[:-1])
     return returnList
 
+def writeToJson(filename,data):
+    with open(filename, "w") as f:
+        jsonStr = json.dumps(data)
+        f.write(jsonStr)
+
+def readFromJson(filename):
+    with open(filename, "r") as f:
+        return json.load(f)
+
 allowedWords = parseWords("allowed_words.txt")
 possibleWords = parseWords("possible_words.txt")
 
 alphabet = {}
 for char in string.ascii_lowercase:
-    alphabet[char] = (0,0,0,0,0,0)
+    alphabet[char] = [0,0,0,0,0,0]
 
 for i in allowedWords:
     for j in range(5):
         alphabet[i[j]][j] += 1
-    
+
 
 for i in alphabet.keys():
     for j in allowedWords:
@@ -29,3 +44,5 @@ allowedWordsDict = {}
 for word in allowedWords:
     allowedWordsDict[word] = (0,0)
 
+writeToJson("data.json", allowedWordsDict)
+print(readFromJson("data.json"))
