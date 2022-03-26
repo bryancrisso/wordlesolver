71bd13dde9c23e7d7fdf7ed3819106ef8a847562
Bryan Abi Karam <bryancrisso@outlook.com>
2022-03-26 17:53:48 +0000

    Finished it off

--- wordleProj/main.py
+++ wordleProj/main.py
@@ -1,4 +1,5 @@
-import string, json
+from math import log2
+import string, json, itertools, functools, time
 
 #generating combinations
 # import itertools
@@ -22,27 +23,94 @@ def readFromJson(filename):
     with open(filename, "r") as f:
         return json.load(f)
 
+#CREATES ALPHABET OF LETTER FREQUENCIES
+def createAlphabet():
+    alphabet = {}
+    for char in string.ascii_lowercase:
+        alphabet[char] = [0,0,0,0,0,0]
+
+    for i in allowedWords:
+        for j in range(5):
+            alphabet[i[j]][j] += 1
+
+    for i in alphabet.keys():
+        for j in allowedWords:
+            if i in j:
+                alphabet[i][5] += 1
+    return alphabet
+
+#key for sorting word list
+def sortKey(e):
+    return e[1]
+
 allowedWords = parseWords("allowed_words.txt")
 possibleWords = parseWords("possible_words.txt")
 
-alphabet = {}
-for char in string.ascii_lowercase:
-    alphabet[char] = [0,0,0,0,0,0]
+alphabet = createAlphabet()
 
-for i in allowedWords:
-    for j in range(5):
-        alphabet[i[j]][j] += 1
+#INITIALISING DICT
+#allowedWordsDict = {}
 
+# for word in allowedWords:
+#     allowedWordsDict[word] = (0,0)
+# writeToJson('data.json', allowedWordsDict)
 
-for i in alphabet.keys():
-    for j in allowedWords:
-        if i in j:
-            alphabet[i][5] += 1
+#CALCULATING COMBINATIONS
+# _choices = [["amb","grn","gry"]]*5
+# combinations = [p for p in itertools.product(*_choices)]
+# combDict = {}
+# for combination in combinations:
+#     _string = ""
+#     for char in combination:
+#         _string += char + "-"
+#     _string = _string[:-1]
+#     combDict[_string] = (0,0)
+# for enum in enumerate(allowedWords):
+#     allowedWordsDict[enum[1]][1] = combDict
 
-allowedWordsDict = {}
+#CALCULATING PROBABILITIES
+# for word in allowedWords:
+#     combinations = allowedWordsDict[word][1]
+#     for combination in combinations:
+#         _combination = combination.split("-")
+#         combinationP = [0,0,0,0,0]
+#         for enum in enumerate(_combination):
+#             if enum[1] == "grn":
+#                 combinationP[enum[0]] = alphabet[word[enum[0]]][enum[0]]/12972
+#             elif enum[1] == "amb":
+#                 combinationP[enum[0]] = (alphabet[word[enum[0]]][5]/12972)-(alphabet[word[enum[0]]][enum[0]]/12972)
+#             elif enum[1] == "gry":
+#                 combinationP[enum[0]] = (12972 - alphabet[word[enum[0]]][5])/12972
+#             combinations[combination][1] = functools.reduce(lambda a,b : a*b, combinationP)
+#     #print(word)
 
+#CALCULATING BITS
+# for word in allowedWords:
+#     combinations = allowedWordsDict[word][1]
+#     for combination in combinations:
+#         combinations[combination][0] = -log2(combinations[combination][1])
+
+#CALCULATING ENTROPY
+# for word in allowedWords:
+#     combinations = allowedWordsDict[word][1]
+#     sum = 0
+#     for combination in combinations:
+#         sum += combinations[combination][0]*combinations[combination][1]
+#     allowedWordsDict[word][0] = sum
+
+timeNow = time.time()
+allowedWordsDict = readFromJson("data.json")
+#print(allowedWordsDict)
+#writeToJson('data.json', allowedWordsDict)
+
+calculatedWords = []
 for word in allowedWords:
-    allowedWordsDict[word] = (0,0)
+    calculatedWords.append((word, allowedWordsDict[word][0]))
+calculatedWords.sort(reverse=True,key=sortKey)
+print(calculatedWords)
 
-writeToJson("data.json", allowedWordsDict)
-print(readFromJson("data.json"))
+with open("calculatedWords.txt", "w") as f:
+    for word in calculatedWords:
+        f.write(word[0] + " " + str(word[1]) + "\n")
+
+print(f"runtime was {time.time()-timeNow} seconds")
